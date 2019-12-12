5e59e25a2b9c023b3fee0000a338ccebb50fd7ef
GameBelial <243387971@qq.com>
2019-12-12 15:39:51 +0800

    增加命令行帮助类。

---
+++ AliCloudDynamicDNS/Utility/ConsoleHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace AliCloudDynamicDNS.Utility
+{
+    public class ConsoleHelper
+    {
+        public const string Normal = nameof(Normal);
+        public const string Error = nameof(Error);
+        
+        public static void WriteDateTimeLine(string msgType, string msg)
+        {
+            WriteMsgType(msgType);
+            Console.WriteLine($"{DateTime.Now:yyyy 年 MM 月 dd 日 HH:mm:ss} - {msg}");
+        }
+
+        private static void WriteMsgType(string msgType)
+        {
+            switch (msgType)
+            {
+                case Normal:
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    Console.Write("[正常] ");
+                    return;
+                case Error:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write("[错误] ");
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    return;
+                default:
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    Console.Write("[正常] ");
+                    return;
+            }
+        }
+    }
+}
