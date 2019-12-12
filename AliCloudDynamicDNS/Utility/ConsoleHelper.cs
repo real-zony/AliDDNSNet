2cb41aa568a1d86f16e50d10526a608cfd73dbf4
GameBelial <243387971@qq.com>
2019-12-12 15:43:01 +0800

    添加两个新的命令行辅助方法。

--- AliCloudDynamicDNS/Utility/ConsoleHelper.cs
+++ AliCloudDynamicDNS/Utility/ConsoleHelper.cs
@@ -6,12 +6,16 @@ namespace AliCloudDynamicDNS.Utility
     {
         public const string Normal = nameof(Normal);
         public const string Error = nameof(Error);
-        
+
         public static void WriteDateTimeLine(string msgType, string msg)
         {
             WriteMsgType(msgType);
             Console.WriteLine($"{DateTime.Now:yyyy 年 MM 月 dd 日 HH:mm:ss} - {msg}");
         }
+
+        public static void WriteMessage(string msg) => WriteDateTimeLine(Normal, msg);
+
+        public static void WriteError(string errMsg) => WriteDateTimeLine(errMsg, errMsg);
 
         private static void WriteMsgType(string msgType)
         {
