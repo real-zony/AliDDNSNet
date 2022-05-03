b872d5534a581546bd2eec2bbe94cc45967d8fbf
Zony <zony@myzony.com>
2022-05-04 00:39:34 +0800

    fix: 调整 IP 获取方法，解决由于在代理后获取 IP 不正确的问题。

--- AliCloudDynamicDNS/Utility/NetworkHelper.cs
+++ AliCloudDynamicDNS/Utility/NetworkHelper.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Net.Http;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace AliCloudDynamicDNS.Utility
@@ -12,19 +13,25 @@ namespace AliCloudDynamicDNS.Utility
             {
                 using (var client = new HttpClient())
                 {
-                    using (var request = new HttpRequestMessage(HttpMethod.Get, "https://api.ipify.org/"))
+                    using (var request = new HttpRequestMessage(HttpMethod.Get, "https://pv.sohu.com/cityjson?ie=utf-8"))
                     {
                         using (var response = await client.SendAsync(request))
                         {
-                            return await response.Content.ReadAsStringAsync();
+                            if (response.IsSuccessStatusCode)
+                            {
+                                var content = await response.Content.ReadAsStringAsync();
+                                return Regex.Match(content, @"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}").Value;
+                            }
+
+                            ConsoleHelper.WriteError($"获取公网IP出错。错误码：{response.StatusCode}");
+                            return "";
                         }
                     }
                 }
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
-                string msg = $"获取公网IP出错，错误原因为：\r\n{ex.Message}";
-                ConsoleHelper.WriteError(ex.Message);
+                ConsoleHelper.WriteError($"获取公网IP出错，错误原因为：\r\n{ex.Message}");
                 return "";
             }
         }
